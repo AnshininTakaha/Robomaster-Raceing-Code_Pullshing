51b076dfdc50de4abc543a63aab485ed10b18be6
Anshinin <759589736@qq.com>
2019-01-12 10:20:39 +0800

    2019.1.12 修改

--- Anshinin Robomaster Infantry/Project/MDK5/Objects/mahonyahrs.d
+++
@@ -1,6 +0,0 @@
-.\objects\mahonyahrs.o: ..\..\Devices\IMU\MahonyAHRS.c
-.\objects\mahonyahrs.o: ..\..\Devices\IMU\MahonyAHRS.h
-.\objects\mahonyahrs.o: ..\..\User\typedefs.h
-.\objects\mahonyahrs.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
-.\objects\mahonyahrs.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\mahonyahrs.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
