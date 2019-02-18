48f803d3eefd6ebec062cb2701ff80c200030b8d
Anshinin <759589736@qq.com>
2019-02-18 17:05:53 +0800

    2019.2.18 新步兵代码模版

---
+++ Anshinin RM_Infantry2/User/Users.h
@@ -0,0 +1,20 @@
+#ifndef _USERS_H
+#define _USERS_H
+
+#define SYSTEM_SUPPORT_OS 1
+
+#pragma anon_unions
+
+
+//#include "binary.h"
+//#include "stm32f4xx.h"
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <stdbool.h>
+//#include <math.h>
+//#include "arm_math.h"
+//#include "core_cm4.h"
+
+
+#endif
